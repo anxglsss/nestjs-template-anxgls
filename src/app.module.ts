4b4929084b798bbcca01d358e6b5e6f1e5042180
madiyarabiken <madiyarabik111@gmail.com>
2025-11-02 22:40:47 +0500

    add new prisma-models, config app, global pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './core/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductModule } from './modules/product/product.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [AuthModule, ProductModule, UserModule],
+  imports: [PrismaModule, AuthModule, ProductModule, UserModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { GlobalValidationPipe } from './shared/pipes/global-validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL'),
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(GlobalValidationPipe);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS Template')
+    .setDescription('NestJS Template API')
+    .setVersion('1.0')
+    .addCookieAuth('access_token')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(configService.getOrThrow('APPLICATION_PORT'), () => {
+    console.log(`Application is running on: ${configService.get('APPLICATION_PORT')}`);
+  });
 }
 void bootstrap();
